19f3d453c7d0763ad76fb036edaf2baa7dacf3a8
Brandon Green <Brandon.Green@knights.ucf.edu>
2021-01-05 18:11:08 -0500

    fixed error on list

---
+++ tutorial/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'The Flash' },
+  { id: 12, name: 'Super-Man' },
+  { id: 13, name: 'Bat-Man' },
+  { id: 14, name: 'Captain America' },
+  { id: 15, name: 'Wolverine' },
+  { id: 16, name: 'Iron Man' },
+  { id: 17, name: 'Spider Man' },
+  { id: 18, name: 'Dr Strange' },
+  { id: 19, name: 'The Hulk' },
+  { id: 20, name: 'Thor' }
+];
