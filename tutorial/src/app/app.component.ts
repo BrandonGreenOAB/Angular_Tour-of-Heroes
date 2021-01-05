c495ca2d6f1a69ce64b3de0f0451289252896cf2
Brandon Green <Brandon.Green@knights.ucf.edu>
2021-01-05 18:38:13 -0500

    added service

--- tutorial/src/app/app.component.ts
+++ tutorial/src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'tutorial';
+  title = 'Tour of Heroes';
   hero = 'selectedHero';
 }
