ef4ff55c82592d4d8a3b6eec1c8c65ff07f96d97
Brandon Green <Brandon.Green@knights.ucf.edu>
2021-01-06 16:30:07 -0500

    added http header

--- tutorial/src/app/hero.service.ts
+++ tutorial/src/app/hero.service.ts
@@ -8,6 +8,9 @@ import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class HeroService {
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
   constructor(
     private http: HttpClient,
     private messageService: MessageService
@@ -33,9 +36,9 @@ export class HeroService {
   //updates the Hero name
   updateHero(hero: Hero): Observable<any> {
     return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
-      tap(_ => this.log(`updated hero id=-${hero.id}`)),
+      tap((_) => this.log(`updated hero id=-${hero.id}`)),
       catchError(this.handleError<any>('updateHero'))
-    )
+    );
   }
   /** Log a HeroService message with the MessageService */
   private log(message: string) {
