f3a713f6540268b87159d2127111e381eb62fd1e
Brandon Green <Brandon.Green@knights.ucf.edu>
2021-01-06 16:29:02 -0500

    added put method to save updated hero names. added error message pipe and local messages for confirmation on save

--- tutorial/src/app/hero.service.ts
+++ tutorial/src/app/hero.service.ts
@@ -15,16 +15,44 @@ export class HeroService {
   getHeroes(): Observable<Hero[]> {
     //message sent after fetching
     this.messageService.add('HeroService: fetched heroes');
-    return this.http.get<Hero[]>(this.heroesUrl);
+    return this.http
+      .get<Hero[]>(this.heroesUrl)
+      .pipe(catchError(this.handleError<Hero[]>('getHeroes', [])));
   }
+
+  private heroesUrl = 'api/heroes'; // URL to web api
+
   getHero(id: number): Observable<Hero> {
-    //message sent after fetching
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find((hero) => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap((_) => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  //updates the Hero name
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=-${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    )
   }
   /** Log a HeroService message with the MessageService */
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
   }
-  private heroesUrl = 'api/heroes'; // URL to web api
+
+  //* Handle Http operation that failed. * Let the app continue. * @param operation - name of the operation that failed * @param result - optional value to return as the observable result
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      //send the error to remote logging infastructure
+      console.error(error); //log to console isntead
+
+      //transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      //let the app keep running by returning an empty result
+      return of(result as T);
+    };
+  }
 }
