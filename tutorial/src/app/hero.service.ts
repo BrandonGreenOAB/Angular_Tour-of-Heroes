4f7c71729d190d5ae3474647607dc5e7dd6b0ecf
Brandon Green <Brandon.Green@knights.ucf.edu>
2021-01-06 16:39:05 -0500

    added add, update, and delete to heroes component. added addHero, updateHero, and deleteHero to hero service

--- tutorial/src/app/hero.service.ts
+++ tutorial/src/app/hero.service.ts
@@ -32,12 +32,29 @@ export class HeroService {
       catchError(this.handleError<Hero>(`getHero id=${id}`))
     );
   }
+  //adds the hero to the server
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
 
   //updates the Hero name
   updateHero(hero: Hero): Observable<any> {
     return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
       tap((_) => this.log(`updated hero id=-${hero.id}`)),
       catchError(this.handleError<any>('updateHero'))
+    );
+  }
+  //deletes hero from server
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap((_) => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
     );
   }
   /** Log a HeroService message with the MessageService */
