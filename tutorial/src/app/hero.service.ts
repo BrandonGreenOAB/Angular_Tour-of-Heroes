990f0b2a36ba1d0e061584937a3101c1e9aaff1a
Brandon Green <Brandon.Green@knights.ucf.edu>
2021-01-05 20:51:02 -0500

    added dashboard and routing to hero detail, dashboard, and hero list

--- tutorial/src/app/hero.service.ts
+++ tutorial/src/app/hero.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { Observable, of } from 'rxjs';
-import {MessageService} from './message.service'
+import { MessageService } from './message.service';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable({ providedIn: 'root' })
 export class HeroService {
   constructor(private messageService: MessageService) {}
   getHeroes(): Observable<Hero[]> {
-    // send a message after fetching the heroes
-    this.messageService.add('HeroService: fetched heroes')
-    return of(HEROES)
+    //message sent after fetching
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+  getHero(id: number): Observable<Hero> {
+    //message sent after fetching
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find((hero) => hero.id === id));
   }
 }
